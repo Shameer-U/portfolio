6db9b5ce52715466d2acbbed1f92ac536067d686
Shameer-U <shameerushahul@gmail.com>
2022-11-08 03:33:22 +0530

    made navbar responsive in mobile screens

--- src/components/header/Header.js
+++ src/components/header/Header.js
@@ -1,8 +1,11 @@
 import './header.css';
 import { useEffect, useState } from 'react';
+import { FaBars } from "react-icons/fa";
+import { MdClose } from "react-icons/md";
 
 const Header = () => {
   const [isScrolled, setIsScrolled] = useState(false);
+  const [navLinkCondition, setnavLinkCondition] = useState('disabled');
 
   useEffect(() => {
     window.onscroll = () => {
@@ -12,18 +15,23 @@ const Header = () => {
     return () => (window.onscroll = null);
   },[])
 
+  const manageNavLinks = () => {
+    setnavLinkCondition((prevState) => { return prevState == "active" ? "disabled" : "active"});
+  }
+
   return (
     <nav className={isScrolled ? "navbar scrolled" : "navbar"}>
         <div className="container">
             <div className="logo">Portfolio</div>
-            <ul className="nav-links">
-                <li><a href="#about">About</a></li>
-                <li><a href="#skills">Skills</a></li>
-                <li><a href="#projects">Projects</a></li>
-                <li><a href="#contact">Contact</a></li>
+            <ul className={"nav-links " + navLinkCondition }>
+                <li className="closeLink" onClick={manageNavLinks}><MdClose/></li>
+                <li onClick={manageNavLinks}><a href="#about">About</a></li>
+                <li onClick={manageNavLinks}><a href="#skills">Skills</a></li>
+                <li onClick={manageNavLinks}><a href="#projects">Projects</a></li>
+                <li onClick={manageNavLinks}><a href="#contact">Contact</a></li>
             </ul>
-            <div className="nav-btn">
-                <i className="fas fa-bars"></i>
+            <div className="nav-btn" onClick={manageNavLinks}>
+                <FaBars className="barIcon" />
             </div>
         </div>
     </nav>
